1ade76ceff574280ad996fd1c3979c0b74e43cef
hema123123 <595333818@qq.com>
2018-04-27 22:41:30 +0800

    1.SVG转成字体格式，上传字体图标 2.模拟数据，模拟get请求 3.安装axios，请求数据

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,30 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <div class="icon-add_circle"></div>
     <router-view/>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+  export default {
+    name: 'App',
+    created(){
+      this.$http.get('http://localhost:8080/api/ratings').then((response) => {
+        console.log(response);
+      })
+    }
+  }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import "common/style/icon.css";
+
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
